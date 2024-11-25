04900dec59362fd39343b7d11b09820058b587f2
Lupan Vasile <137153217+zqplugins@users.noreply.github.com>
2024-11-25 14:17:11 +0200

    added components

---
+++ Components/SignIn/SignIn.tsx
@@ -0,0 +1,140 @@
+import { framer } from "framer-plugin";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import AlertBlock, { AlertMessage } from "../AlertBlock/AlertBlock";
+import axios from "axios";
+import "./style.css";
+
+const API_BASE_URL =
+  "https://zeroqodeplugins.bubbleapps.io/version-test/api/1.1/wf";
+
+const SignIn = () => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
+  const [alert, setAlert] = useState<AlertMessage | null | undefined>(null);
+  const navigate = useNavigate();
+
+  const showError = (message: string) => {
+    setAlert({ message, type: "error", time: null });
+    setLoading(false);
+  };
+
+  const handleApiRequest = async (
+    endpoint: string,
+    data: Record<string, any>,
+    onSuccess: (res: any) => void
+  ) => {
+    try {
+      const res = await axios.post(`${API_BASE_URL}/${endpoint}`, data);
+      if (res.data.status === "success") {
+        onSuccess(res);
+      } else {
+        showError("Unexpected API response.");
+      }
+    } catch (error: any) {
+      showError(error?.response?.data?.message || "An error occurred.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleSignIn = async () => {
+    if (!email || !password) {
+      showError("Some fields are blank");
+      return;
+    }
+
+    setLoading(true);
+
+    const data = { email, password };
+
+    await handleApiRequest("sign_in", data, async (res) => {
+      const response = res.data.response;
+      const secondsNow = Math.floor(Date.now() / 1000);
+      const tokenValidTimestamp = secondsNow + response.expires;
+
+      await Promise.all([
+        framer.setPluginData("UserEmail", email),
+        framer.setPluginData("UserID", response.user_id),
+        framer.setPluginData("UserToken", response.token),
+        framer.setPluginData(
+          "UserIsSubscribed",
+          response["is subscribed?"].toString()
+        ),
+        framer.setPluginData("UserExpiresIn", tokenValidTimestamp.toString()),
+        framer.setPluginData(
+          "UserSubscribtionID",
+          response["subscription ID"] || ""
+        ),
+        framer.setPluginData(
+          "UserSubscribtionDate",
+          response["subscription date"].toString() || ""
+        ),
+      ]);
+
+      setAlert(null);
+
+      navigate(response["is subscribed?"] ? "/PluginWork" : "/PaySubscription");
+    });
+  };
+
+  const handleSignUp = () => navigate("/SignUp");
+
+  const handleForgotPass = async () => {
+    if (!email) {
+      showError("Please enter your email");
+      return;
+    }
+
+    setLoading(true);
+
+    const data = { email };
+
+    await handleApiRequest("forgot_password", data, () => {
+      setAlert({
+        message: "The password reset email has been sent to your inbox.",
+        type: "info",
+        time: 2000,
+      });
+    });
+  };
+
+  return (
+    <main id="SignIn">
+      <button className="button-back" onClick={() => navigate(-1)}>
+        <i className="fa fa-lg fa-angle-left"></i>
+        Back
+      </button>
+      <h2>Account Login</h2>
+      <input
+        className="input-text"
+        type="text"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        className="input-text"
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button
+        className="framer-button-primary"
+        onClick={handleSignIn}
+        disabled={loading}
+      >
+        {loading ? "Loading..." : "Login"}
+      </button>
+      <div className="div-row">
+        <span onClick={handleSignUp}>Don't have an account?</span>
+        <span onClick={handleForgotPass}>Forgot password?</span>
+      </div>
+      <AlertBlock Alert={alert} setAlert={setAlert} />
+    </main>
+  );
+};
+
+export default SignIn;
---
+++ Components/SignUp/SignUp.tsx
@@ -0,0 +1,129 @@
+import { framer } from "framer-plugin";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import AlertBlock, { AlertMessage } from "../AlertBlock/AlertBlock";
+import axios from "axios";
+import "./style.css";
+
+const MyComponent = () => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [confirmPassword, setConfirmPassword] = useState<string>("");
+  const [alert, setAlert] = useState<AlertMessage | null>();
+  const [loading, setLoading] = useState<boolean>();
+  const navigate = useNavigate();
+
+  const validateEmail = (email: string) => {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+  };
+
+  const handleSignUp = async () => {
+    setLoading(true);
+    if (!email || !password || !confirmPassword) {
+      setAlert({ message: "Some fields blank", type: "error", time: null });
+      setLoading(false);
+      return;
+    } else if (password != confirmPassword) {
+      setAlert({ message: "Passwords don't match", type: "error", time: null });
+      setLoading(false);
+      return;
+    } else if (!validateEmail(email)) {
+      setAlert({ message: "Email is not correct", type: "error", time: null });
+      setLoading(false);
+      return;
+    }
+
+    let data = {
+      email,
+      password,
+      confirmPassword,
+    };
+    axios
+      .post(
+        "https://zeroqodeplugins.bubbleapps.io/version-test/api/1.1/wf/sign_up",
+        data
+      )
+      .then(async (res) => {
+        if (res.data.status === "success") {
+          await framer.setPluginData("UserEmail", email);
+          await framer.setPluginData("UserID", res.data.response.user_id);
+          await framer.setPluginData("UserToken", res.data.response.token);
+          await framer.setPluginData(
+            "UserIsSubscribed",
+            res.data.response["is subscribed?"].toString()
+          );
+          let seconds_now = Math.floor(Date.now() / 1000);
+          let tokenValidTimestamp = seconds_now + res.data.response.expires;
+          await framer.setPluginData(
+            "UserExpiresIn",
+            tokenValidTimestamp.toString()
+          );
+          setAlert(null);
+          setLoading(false);
+
+          if (res.data.response["is subscribed?"]) {
+            console.log("");
+          } else {
+            navigate("/PaySubscription");
+          }
+        }
+      })
+      .catch((error) => {
+        setAlert({
+          message: JSON.stringify(error?.response?.data?.message),
+          type: "error",
+          time: null,
+        });
+        setLoading(false);
+      });
+  };
+
+  const handleSignIn = async () => {
+    navigate("/SignIn");
+  };
+
+  return (
+    <main id="SignUp">
+      <button className="button-back" onClick={() => navigate(-1)}>
+        <i className="fa fa-lg fa-angle-left"></i>
+        Back
+      </button>
+      <h2>Account Register</h2>
+      <input
+        className="input-text"
+        type="text"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        className="input-text"
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <input
+        className="input-text"
+        type="password"
+        placeholder="Confirm Password"
+        value={confirmPassword}
+        onChange={(e) => setConfirmPassword(e.target.value)}
+      />
+      <button
+        disabled={loading}
+        className="framer-button-primary"
+        onClick={handleSignUp}
+      >
+        {loading ? "Loading" : "Sign Up"}
+      </button>
+      <div className="div-row">
+        <span onClick={handleSignIn}>Already have an account?</span>
+      </div>
+      <AlertBlock Alert={alert} setAlert={setAlert} />
+    </main>
+  );
+};
+
+export default MyComponent;
