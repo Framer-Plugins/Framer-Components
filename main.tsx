e3e268e99981e49d8a228b05e2e5a8f96b4783a9
Lupan Vasile <137153217+zqplugins@users.noreply.github.com>
2024-11-26 12:10:18 +0200

    1

--- main.tsx
+++ main.tsx
@@ -1,3 +1,4 @@
-export { default as AlertBlock, AlertMessage } from './Components/AlertBlock/AlertBlock';
-export { default as SignIn } from './Components/SignIn/SignIn';
-export { default as SignUp } from './Components/SignUp/SignUp';
+export { default as AlertBlock } from "./Components/AlertBlock/AlertBlock";
+export type { AlertMessage } from "./Components/AlertBlock/AlertBlock";
+export { default as SignIn } from "./Components/SignIn/SignIn";
+export { default as SignUp } from "./Components/SignUp/SignUp";
