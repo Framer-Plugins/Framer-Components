45a7ca574c5f372473b212c0d37b5774eebf0968
Lupan Vasile <137153217+zqplugins@users.noreply.github.com>
2024-11-28 11:42:56 +0200

    1

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/main.tsx",
+  output: {
+    filename: "ZqFramerComponents.js",
+    path: path.resolve(__dirname, "dist"),
+    library: "ZqFramerComponents",
+    libraryTarget: "umd",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts)x?$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+  },
+  externals: {
+    react: "React",
+    "react-dom": "ReactDOM",
+  },
+};
