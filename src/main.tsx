2db742aedcfcdedc4969a4a8238875c0af60b8c0
Lupan Vasile <137153217+zqplugins@users.noreply.github.com>
2024-11-28 15:22:14 +0200

    1

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,22 @@
 
-export { default as AlertBlock } from "./Components/AlertBlock/AlertBlock";
-export type { AlertMessage } from "./Components/AlertBlock/AlertBlock";
+/* export { default as AlertBlock } from "./Components/AlertBlock/AlertBlock"; */
+/* export type { AlertMessage } from "./Components/AlertBlock/AlertBlock"; */
 /* export { default as SignIn } from "./Components/SignIn/SignIn";
 export { default as SignUp } from "./Components/SignUp/SignUp";
  */
+
+import React from "react";
+import ReactDOM from "react-dom";
+import AlertBlock from "./Components/AlertBlock/AlertBlock";
+import type AlertMessage from "./Components/AlertBlock/AlertBlock";
+
+const PNIReact = React;
+const PNIReactDOM = ReactDOM;
+
+export default {
+  PNIReact,
+  PNIReactDOM,
+  AlertBlock,
+}
+
+export type {AlertMessage}
