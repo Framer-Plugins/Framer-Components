3db763b5da6992065a837115a178dfe004899bdd
Lupan Vasile <137153217+zqplugins@users.noreply.github.com>
2024-11-30 19:32:23 +0200

    1

--- src/main.tsx
+++ src/main.tsx
@@ -5,4 +5,4 @@ export { default as SignIn } from "./Components/SignIn/SignIn";
 export { default as SignUp } from "./Components/SignUp/SignUp";
 export { default as StartPage } from "./Components/StartPage/StartPage";
 export { default as PaySubscription } from "./Components/PaySubscription/PaySubscription";
-export { default as Home } from "./Components/Home/Home";
+/* export { default as Home } from "./Components/Home/Home"; */
